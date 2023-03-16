445870b79f4a16ad2ce0b6b3cdf8b71b8f8ead19
shoaib <113289146+shoaib171@users.noreply.github.com>
2023-03-16 12:40:20 -0700

    login-verified

--- src/App.js
+++ src/App.js
@@ -9,15 +9,22 @@ import Contact from "./Components/Contact";
 import About from "./Components/About";
 import ProductDetail from "./Components/ProductDetail";
 import Cart from "./Components/Cart";
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
 function App() {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/cart"
+          element={isAuthenticated ? <Cart /> : <Navigate to="/products" />}
+        />
         <Route path="/products" element={<Products />} />
         <Route path="/productDetail/:id" element={<ProductDetail />} />
+
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
